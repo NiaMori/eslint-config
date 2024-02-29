80b11bce5d790fbfac76050c8f31fb6d0f7ae835
NiaMori <Hellc5261@gmail.com>
2024-02-29 17:32:49 +0800

    feat: move some rules to antfu overrides and add react auto-detection

--- index.js
+++ index.js
@@ -1,24 +1,125 @@
-import antfu from '@antfu/eslint-config'
 import fs from 'node:fs'
-import { object, boolean } from 'zod'
+import antfu from '@antfu/eslint-config'
+import { boolean, object } from 'zod'
+import { isPackageExists } from 'local-pkg'
 
-import pluginAntfu from 'eslint-plugin-antfu'
-import pluginTs from '@typescript-eslint/eslint-plugin'
-import pluginStylistic from '@stylistic/eslint-plugin'
 import pluginJsonc from 'eslint-plugin-jsonc'
-// @ts-expect-error eslint-plugin-no doesn't have types
+
+// @ts-expect-error eslint-plugin-n doesn't have types
 import pluginNode from 'eslint-plugin-n'
+
+/**
+ * @type {() => Promise<import('@antfu/eslint-config').FlatConfigItem[]>}
+ */
+async function reactConfigs() {
+  const isAllowConstantExport = ['vite'].some(i => isPackageExists(i))
+
+  // @ts-expect-error eslint-plugin-react doesn't have types
+  const pluginReact = await import('eslint-plugin-react').then(it => it.default || it)
+  // @ts-expect-error eslint-plugin-react-hooks doesn't have types
+  const pluginReactHooks = await import('eslint-plugin-react-hooks').then(it => it.default || it)
+  // @ts-expect-error eslint-plugin-react-refresh doesn't have types
+  const pluginReactRefresh = await import('eslint-plugin-react-refresh').then(it => it.default || it)
+
+  return [
+    {
+      name: 'niamori:react:setup',
+      plugins: {
+        'react': pluginReact,
+        'react-hooks': pluginReactHooks,
+        'react-refresh': pluginReactRefresh,
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    },
+    {
+      files: ['**/*.?([cm])jsx', '**/*.?([cm])tsx'],
+      languageOptions: {
+        parserOptions: {
+          ecmaFeatures: {
+            jsx: true,
+          },
+        },
+      },
+      name: 'niamori:react:rules',
+      rules: {
+        // recommended rules react-hooks
+        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/rules-of-hooks': 'error',
+
+        // react refresh
+        'react-refresh/only-export-components': [
+          'warn',
+          { allowConstantExport: isAllowConstantExport },
+        ],
+
+        // recommended rules react
+        'react/display-name': 'error',
+        'react/jsx-key': 'error',
+        'react/jsx-no-comment-textnodes': 'error',
+        'react/jsx-no-duplicate-props': 'error',
+        'react/jsx-no-target-blank': 'error',
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/no-children-prop': 'error',
+        'react/no-danger-with-children': 'error',
+        'react/no-deprecated': 'error',
+        'react/no-direct-mutation-state': 'error',
+        'react/no-find-dom-node': 'error',
+        'react/no-is-mounted': 'error',
+        'react/no-render-return-value': 'error',
+        'react/no-string-refs': 'error',
+        'react/no-unescaped-entities': 'error',
+        'react/no-unknown-property': 'error',
+        'react/no-unsafe': 'off',
+        'react/prop-types': 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/require-render-return': 'error',
+
+        // disable following rules for typescript
+        'react/jsx-no-undef': 'off',
+        'react/prop-type': 'off',
+      },
+    },
+  ]
+}
 
 export async function pleaseConfigESLintForMe() {
   /**
    * @type {import('@antfu/eslint-config').OptionsConfig}
    */
   const antfuOptions = {
-    stylistic: true,
+    stylistic: {
+      overrides: {
+        'antfu/top-level-function': 'off',
+
+        'curly': ['error', 'all'],
+
+        'style/brace-style': ['error', '1tbs'],
+
+        'style/jsx-quotes': ['error', 'prefer-single'],
+        'style/jsx-closing-bracket-location': ['error', 'line-aligned'],
+        'style/jsx-closing-tag-location': 'error',
+        'style/jsx-equals-spacing': ['error', 'always'],
+        'style/jsx-indent': ['error', 2, { checkAttributes: true, indentLogicalExpressions: true }],
+        'style/jsx-indent-props': ['error', 2],
+        'style/jsx-sort-props': ['error', { callbacksLast: true, shorthandFirst: true, reservedFirst: true }],
+        'style/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
+        'style/jsx-curly-spacing': ['error', { when: 'never', children: true }],
+      },
+    },
+
     typescript: {
       tsconfigPath: 'tsconfig.json',
+      overrides: {
+        'ts/no-require-imports': 'off',
+        'ts/no-redeclare': 'off',
+        'ts/consistent-type-definitions': 'off',
+      },
     },
-    react: true
   }
 
   const antfuConfigs = await antfu(antfuOptions)
@@ -40,58 +141,15 @@ export async function pleaseConfigESLintForMe() {
     })
   }
 
+  if (isPackageExists('react')) {
+    myConfigs.push(...await reactConfigs())
+  }
+
   myConfigs.push(
-    {
-      name: 'niamori:preferences:stylistic',
-
-      plugins: {
-        antfu: pluginAntfu,
-        stylistic: pluginStylistic,
-      },
-
-      rules: {
-        'antfu/top-level-function': 'off',
-
-        'curly': ['error', 'all'],
-
-        'style/brace-style': ['error', '1tbs'],
-
-        'style/jsx-quotes': ['error', 'prefer-single'],
-        'style/jsx-closing-bracket-location': ['error', 'line-aligned'],
-        'style/jsx-closing-tag-location': 'error',
-        'style/jsx-equals-spacing': ['error', 'always'],
-        'style/jsx-indent': ['error', 2, { checkAttributes: true, indentLogicalExpressions: true }],
-        'style/jsx-indent-props': ['error', 2],
-        'style/jsx-sort-props': ['error', { callbacksLast: true, shorthandFirst: true, reservedFirst: true }],
-        'style/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }],
-        'style/jsx-curly-spacing': ['error', { when: 'never', children: true }],
-      },
-    },
-
-    {
-      name: 'niamori:preferences:typescript',
-
-      files: ['**/*.?([cm])[jt]s?(x)'],
-
-      plugins: {
-        ts: pluginTs,
-      },
-
-      rules: {
-        'ts/no-require-imports': 'off',
-        'ts/no-redeclare': 'off',
-        'ts/consistent-type-definitions': 'off',
-      },
-    },
-
     {
       name: 'niamori:preferences:typescript:javascript-overrides',
 
       files: ['**/*.js', '**/*.cjs'],
-
-      plugins: {
-        ts: pluginTs,
-      },
 
       rules: {
         'ts/no-unsafe-assignment': 'off',
